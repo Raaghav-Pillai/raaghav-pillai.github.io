a043d00e3fce9053590c1bdf5c38b9c7186abaa0
Raaghav-Pillai <raaghavpillai@gmail.com>
2025-04-16 12:01:29 -0500

    improved design

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    }
-  });
-}, {
-  threshold: 0.1
-});
-
-sections.forEach(section => {
-  observer.observe(section);
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, { threshold: 0.1 });
+  
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });  
